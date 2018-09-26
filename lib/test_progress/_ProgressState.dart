b55b52a8c224fdf42d77caea736406ed0ad4bfeb
liupengfei <2885726384@qq.com>
2018-09-26 15:05:47 +0800

    自定义进度条

--- lib/test_progress/_ProgressState.dart
+++ lib/test_progress/_ProgressState.dart
@@ -1,47 +1,46 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/animation.dart';
 import 'dart:async';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 class MyProgress extends StatefulWidget {
-  final int count;
-  final int milliseconds;
   final Size size;
   final Color color;
+  final int count;
+  final int milliseconds;
 
-  MyProgress({@required this.size, this.milliseconds: 300, this.color: Colors
+  const MyProgress({@required this.size, this.milliseconds: 300, this.color: Colors
       .green, this.count: 4});
 
   @override
-  State<StatefulWidget> createState() {
-    return new _ProgressState();
-  }
+  State<StatefulWidget> createState() => _ProgressState();
+
 }
 
 class _ProgressState extends State<MyProgress> with TickerProviderStateMixin {
-  List<Animation<double>> animators = [];
-  List<AnimationController> _animationControllers = [];
+
+  List<Animation<double>>animators = [];
+  List<AnimationController>_animationControllers = [];
 
   @override
   void initState() {
     super.initState();
     for (int i = 0; i < widget.count; i++) {
-      var animationController = new AnimationController(
-          vsync: this,
+
+      var animationController = new AnimationController(vsync: this,
           duration: Duration(milliseconds: widget.milliseconds * widget.count));
-      animationController.value = 0.8 * i / widget.count;
+      animationController.value=0.8*i/widget.count;
       _animationControllers.add(animationController);
-      Animation<double> animation =
-          new Tween(begin: 0.1, end: 1.9).animate(animationController);
+      Animation<double> animation = new Tween(begin: 0.1, end: 1.9).animate(
+          animationController);
       animators.add(animation);
     }
-
     animators[0].addListener(_change);
-
     try {
-      var mi = (widget.milliseconds ~/ (animators.length - 1));
+      var mi = (widget.milliseconds~/(1*animators.length-1));
       for (int i = 0; i < animators.length; i++) {
-        print((mi * i).toString());
-        dodelay(_animationControllers[i], mi * i);
+        print(( mi*i).toString());
+        dodelay(_animationControllers[i], mi*i);
       }
     } on Exception {
 
@@ -61,17 +60,16 @@ class _ProgressState extends State<MyProgress> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return new CustomPaint(
-      painter: _Progress(
-          animators: animators, color: widget.color, count: widget.count),
-      size: widget.size,
-    );
+    return new CustomPaint(painter: _Progress(
+        animators: animators, color: widget.color, count: widget.count),
+      size: widget.size,);
   }
 
   @override
   void dispose() {
     super.dispose();
     animators[0].removeListener(_change);
+//    _animationControllers[0].dispose();
     for (AnimationController _animationCountroller in _animationControllers) {
       _animationCountroller.dispose();
     }
@@ -81,18 +79,20 @@ class _ProgressState extends State<MyProgress> with TickerProviderStateMixin {
 class _Progress extends CustomPainter {
   final Color color;
   final int count;
-  final List<Animation<double>> animators;
+  final List<Animation<double>>animators;
 
   const _Progress({this.animators, this.color, this.count});
 
+  @override
   void paint(Canvas canvas, Size size) {
     var radius = size.width / (3 * count + 1);
     var paint = new Paint()
       ..color = color
       ..style = PaintingStyle.fill;
-    for (int i = 0; i < count + 1; i++) {
+    for (int i = 1; i < count + 1; i++) {
       double value = animators[i - 1].value;
-      canvas.drawCircle(new Offset(radius * i * 3 - radius, size.height / 2),
+      canvas.drawCircle(
+          new Offset(radius * i * 3 - radius, size.height / 2),
           radius * (value > 1 ? (2 - value) : value), paint);
     }
   }
@@ -101,4 +101,5 @@ class _Progress extends CustomPainter {
   bool shouldRepaint(CustomPainter oldDelegate) {
     return oldDelegate != this;
   }
+
 }
